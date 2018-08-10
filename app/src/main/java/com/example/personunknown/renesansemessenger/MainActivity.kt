35418807fe61b5f1c450195103b38366253f349b
Renesanse <dsrenesanse@gmail.com>
2018-08-10 17:45:34 +0300

    EGGI BLYAT

--- app/src/main/java/com/example/personunknown/renesansemessenger/MainActivity.kt
+++ app/src/main/java/com/example/personunknown/renesansemessenger/MainActivity.kt
@@ -9,5 +9,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val fragmentRegister = FragmentRegister()
         supportFragmentManager.beginTransaction().add(R.id.fragment_container,fragmentRegister).commit()
+        //
     }
 }
